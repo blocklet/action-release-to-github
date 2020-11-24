f53e6a3f983467ff21cdf7b1daf36a527ec08a1a
linchen1987 <linchen.1987@foxmail.com>
2020-11-24 15:25:45 +0800

    v1.0.0

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # action-release-to-github
+
+Use github action to release blocklet to Github Release
+
+Example workflow
+
+```yml
+on:
+  push:
+    # Sequence of patterns matched against refs/tags
+    tags:
+      - 'v*' # Push events to matching v*, i.e. v1.0, v20.15.10
+
+name: Create Release
+
+jobs:
+  Deploy:
+    runs-on: ubuntu-latest
+    steps:
+      - name: Checkout code
+        uses: actions/checkout@v2
+      - name: Build
+        run: <build_your_blocklet> # after build, use `abtnode bundle --create-release` to bundle your blocklet
+      - name: Release to Github
+        uses: blocklet/action-release-to-github@v1.0.0
+        with:
+          token: ${{ secrets.GITHUB_TOKEN }}
+```
---
+++ index.js
@@ -0,0 +1,94 @@
+const path = require('path');
+const shell = require('shelljs');
+const fs = require('fs')
+const github = require('@actions/github');
+const core = require('@actions/core');
+
+
+const run = async () => {
+  const githubToken = core.getInput('token', {required: true});
+  const [owner, repo] = process.env.GITHUB_REPOSITORY.split('/')
+  
+  shell.exec('git config --local user.name "ArcBlock Robot"');
+
+  // get blocklet version
+  let version = ''
+  const file = path.join(process.cwd(), '.blocklet/release/blocklet.json')
+  if (fs.existsSync(file)) {
+    version = require(file).version;
+  } else {
+    throw new Error('Missing file at .blocklet/release/blocklet.json')
+  }
+
+  // upload assets to github release
+  console.log(`Create release: ${owner}, repo: ${repo}, tag: ${version}`)
+  const octokit = github.getOctokit(githubToken);
+  const release = await octokit.repos.createRelease({
+    owner,
+    repo,
+    tag_name: version,
+  })
+
+  console.log(`Start upload assets to release. id: ${release.data.id}, upload_url: ${release.data.upload_url}`);
+
+  const files = fs.readdirSync('.blocklet/release');
+
+  try {
+    for (let i = 0; i < files.length; i++) {
+      const f = files[i];
+      const file = `.blocklet/release/${f}`;
+      const stat = fs.statSync(file)
+      const file_size = stat.size
+      const file_bytes = fs.readFileSync(file)
+      console.log(`Uploading ${f}...`)
+      response = await octokit.repos.uploadReleaseAsset({
+        name: f,
+        data: file_bytes,
+        url: release.data.upload_url,
+        headers: {
+          'content-type': 'binary/octet-stream',
+          'content-length': file_size
+        },
+      })
+      console.log(`Upload ${f} success`);
+    }
+    console.log('Upload assets success');
+  } catch (err) {
+    console.log('Upload assets failed');
+    console.log(`Delete release: ${release.data.id}`)
+    await octokit.repos.deleteRelease({
+      owner,
+      repo,
+      release_id: release.data.id,
+    });
+    throw err;
+  }
+
+  // update readme
+  console.log('Update README.md...');
+  const readme = fs.readFileSync('./README.md').toString();
+  const link = `https://install.arcblock.io/?action=blocklet-install&meta_url=https%3A%2F%2Fgithub.com%2F${owner}%2F${repo}%2Freleases%2Fdownload%2F${version}%2Fblocklet.json`;
+  const regex = /download%2F(\d+\.\d+\.\d+)%2Fblocklet\.json/;
+  let newReadme;
+  const paragraph =  `## Install on my ABT Node
+
+[![Install on my ABT Node](https://raw.githubusercontent.com/blocklet/development-guide/main/assets/install_on_abtnode.svg)](${link})
+
+`
+  if (!regex.test(readme)) {
+    const index = readme.indexOf('##')
+    newReadme = readme.substring(0, index) + paragraph + readme.substring(index)
+  } else {
+    newReadme = readme.replace(regex, `download%2F${version}%2Fblocklet.json`)
+  }
+
+  fs.writeFileSync('./README.md', newReadme)
+  shell.exec(`git add README.md`);
+  shell.exec('git commit -n -m "[skip ci] Update README.md"');
+  shell.exec(`git push`);
+}
+
+run().catch(err => {
+  console.error(err)
+  process.exit(1);
+});
