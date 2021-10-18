74fbd2c8c47d2ae01280d6df3dcd6345eb019340
LancelotLewis <lancelot_lewis@163.com>
2021-10-19 00:21:49 +0800

    v1.1.4

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ if (skip === 'true') {
 
 const run = async () => {
   const filePath = core.getInput('file-path');
-  const workingDirectory = core.getInput('workgin-directory');
+  const workingDirectory = core.getInput('working-directory');
   const githubToken = core.getInput('token', { required: true });
 
   const cdRes = shell.cd(workingDirectory);
